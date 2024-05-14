d41ff51edbf9ed904b2d15e0d61e12a50188e312
DayvsonDvs <dayvson_dvs@hotmail.com>
2024-05-14 15:06:59 -0300

    fix: dir images for diploma

--- components/molecules/Gallery.vue
+++ components/molecules/Gallery.vue
@@ -7,18 +7,18 @@
 
 <script setup lang="ts">
 const listDiploma = [
-  { title: 'Bacharelado em Sistema de Informação', src: '/_nuxt/assets/imgs/Graduação.png', duration: '4 anos', conclusion: 'Recife 10 de dezembro de 2018' },
-  { title: 'Take Blip Fullstack Developer', src: '/_nuxt/assets/imgs/Takeblip.png', duration: '97 horas', conclusion: '12 de junho de 2021' },
-  { title: 'MRV .NET Developer', src: '/_nuxt/assets/imgs/MRV.png', duration: '90 horas', conclusion: '19 de junho de 2021' },
-  { title: 'LocalizaLabs .NET Developer', src: '/_nuxt/assets/imgs/Localiza.png', duration: '90 horas', conclusion: '18 de maio de 2021' },
-  { title: 'Formação Node.js', src: '/_nuxt/assets/imgs/Nodejs.png', duration: '30 horas', conclusion: '6 de maio de 2020' },
-  { title: 'C# Completo Programação Orientada a Objetos', src: '/_nuxt/assets/imgs/dotnetcompleto.png', duration: '34,5 horas', conclusion: '28 de março de 2020' },
-  { title: 'Headers Animados e Responsivos', src: '/_nuxt/assets/imgs/Html_responsivo.png', duration: '5 horas', conclusion: '8 de maio de 2020' },
-  { title: 'HTML Básico', src: '/_nuxt/assets/imgs/Html.png', duration: '11 horas', conclusion: '14 de março de 2016' },
-  { title: 'SQL Server', src: '/_nuxt/assets/imgs/SqlServer.png', duration: '31 horas', conclusion: '14 de março de 2016' },
-  { title: 'Inovando com CSS', src: '/_nuxt/assets/imgs/InovandoCss.png', duration: '26 horas', conclusion: '17 de março de 2016' },
-  { title: 'Modelagem de Dados', src: '/_nuxt/assets/imgs/ModelagemDados.png', duration: '35 horas', conclusion: '09 de março de 2016' },
-  { title: 'Lógica de Programação', src: '/_nuxt/assets/imgs/LogicaProgramação.png', duration: '14 horas', conclusion: '19 de junho de 2015' },
+  { title: 'Bacharelado em Sistema de Informação', src: '_nuxt/public/imgs/Graduação.png', duration: '4 anos', conclusion: 'Recife 10 de dezembro de 2018' },
+  { title: 'Take Blip Fullstack Developer', src: '_nuxt/public/imgs/Takeblip.png', duration: '97 horas', conclusion: '12 de junho de 2021' },
+  { title: 'MRV .NET Developer', src: '_nuxt/public/imgs/MRV.png', duration: '90 horas', conclusion: '19 de junho de 2021' },
+  { title: 'LocalizaLabs .NET Developer', src: '_nuxt/public/imgs/Localiza.png', duration: '90 horas', conclusion: '18 de maio de 2021' },
+  { title: 'Formação Node.js', src: '_nuxt/public/imgs/Nodejs.png', duration: '30 horas', conclusion: '6 de maio de 2020' },
+  { title: 'C# Completo Programação Orientada a Objetos', src: '_nuxt/public/imgs/dotnetcompleto.png', duration: '34,5 horas', conclusion: '28 de março de 2020' },
+  { title: 'Headers Animados e Responsivos', src: '_nuxt/public/imgs/Html_responsivo.png', duration: '5 horas', conclusion: '8 de maio de 2020' },
+  { title: 'HTML Básico', src: '_nuxt/public/imgs/Html.png', duration: '11 horas', conclusion: '14 de março de 2016' },
+  { title: 'SQL Server', src: '_nuxt/public/imgs/SqlServer.png', duration: '31 horas', conclusion: '14 de março de 2016' },
+  { title: 'Inovando com CSS', src: '_nuxt/public/imgs/InovandoCss.png', duration: '26 horas', conclusion: '17 de março de 2016' },
+  { title: 'Modelagem de Dados', src: '_nuxt/public/imgs/ModelagemDados.png', duration: '35 horas', conclusion: '09 de março de 2016' },
+  { title: 'Lógica de Programação', src: '_nuxt/public/imgs/LogicaProgramação.png', duration: '14 horas', conclusion: '19 de junho de 2015' },
 ]
 
 </script>
